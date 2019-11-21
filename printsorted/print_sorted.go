4bc454ca9bfc001df7bc3650341a5c4c565c265e
VSKut <me@vskut.ru>
2019-11-21 16:37:12 +0300

    sortMap replace map to slice

--- printsorted/print_sorted.go
+++ printsorted/print_sorted.go
@@ -13,16 +13,14 @@ func PrintSorted(m map[int]string) {
 	fmt.Println()
 }
 
-func sortMap(m map[int]string) map[int]string {
-	r := map[int]string{}
-	k := make([]int, 0, len(m))
+func sortMap(m map[int]string) []string {
+	k, r := make([]int, 0, len(m)), make([]string, 0, len(m))
 	for index := range m {
 		k = append(k, index)
 	}
 	sort.Ints(k)
-	for _, index := range k {
-
-		r[index] = m[index]
+	for _, value := range k {
+		r = append(r, m[value])
 	}
 	return r
 }
