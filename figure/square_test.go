8878fe0ebe716fb9a0259faab5fc9014290c090e
VSKut <me@vskut.ru>
2019-11-22 15:46:53 +0300

    square_test init

---
+++ figure/square_test.go
@@ -0,0 +1,19 @@
+package figure
+
+import "testing"
+
+func TestSquare_Area(t *testing.T) {
+	c := Square{Width: 10}
+
+	if a := c.Area(); a != float64(100) {
+		t.Error("Square{Width: 10}.Area() != 100. Returned: ", a)
+	}
+}
+
+func TestSquare_Perimeter(t *testing.T) {
+	c := Square{Width: 10}
+
+	if a := c.Perimeter(); a != float64(40) {
+		t.Error("Square{Width: 10}.Perimeter() != 40. Returned: ", a)
+	}
+}
