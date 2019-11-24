5d6aae4543833d019da853686f4654a1b4b2c954
VSKut <me@vskut.ru>
2019-11-24 18:51:21 +0300

    method renaming sortMap -> sortByKeys

--- printsorted/print_sorted.go
+++ printsorted/print_sorted.go
@@ -6,14 +6,14 @@ import (
 )
 
 func PrintSorted(m map[int]string) {
-	for _, v := range sortMap(m) {
+	for _, v := range sortByKeys(m) {
 		fmt.Print(v, " ")
 	}
 
 	fmt.Println()
 }
 
-func sortMap(m map[int]string) []string {
+func sortByKeys(m map[int]string) []string {
 	k, r := make([]int, 0, len(m)), make([]string, 0, len(m))
 	for index := range m {
 		k = append(k, index)
